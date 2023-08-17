10699b40008b61c7fd542d6ef8e5d10b87ab7026
Lizzy Mills <lizzyhmills@gmail.com>
2023-08-17 22:59:11 +0100

    Iterated index images

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,9 +8,11 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-     if @bookmark.save
+    if @bookmark.save
       redirect_to list_path(@list)
-     end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,8 @@
 require "json"
 require "open-uri"
 
+Movie.destroy_all
+
 url = 'https://tmdb.lewagon.com/movie/top_rated'
 user_serialized = URI.open(url).read
 movies = JSON.parse(user_serialized)
@@ -17,18 +19,7 @@ movies["results"].each do |movie|
   Movie.create(
      title: movie["title"],
      overview: movie["overview"],
-     poster_url: movie["poster_path"]
+     poster_url: "https://tmdb.lewagon.com/#{movie["poster_path"]}"
   )
   puts "Creating #{movie["title"]}"
 end
-
-
-# title = movie(title: movie["title"])
-# movie(overview: movie["overview"])
-# movie(poster_url: movie["poster_path"])
-
-
-# Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-# Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-# Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-# Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
